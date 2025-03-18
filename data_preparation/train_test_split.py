ab1ceeca97a6c4e412b1e150cf149be9835bd89b
Rishabh Narayanan <rishabh.r.narayanan@gmail.com>
2025-03-18 10:34:40 -0400

    Modified train test split

--- data_preparation/train_test_split.py
+++ data_preparation/train_test_split.py
@@ -3,15 +3,12 @@ import pandas as pd
 
 df = pd.read_csv("./data/result.csv")
 
-X_train, X_test = train_test_split(df, test_size=0.25, random_state=42, stratify=df["source"])
+X_train, X_test = train_test_split(
+    df, test_size=0.25, random_state=42, stratify=df["source"]
+)
 
-X_train["type"] = "train"
-X_test["type"] = "test"
+X_train.sort_index(inplace=True)
+X_test.sort_index(inplace=True)
 
-combined = pd.concat([X_train, X_test], axis=0)
-combined.sort_index(inplace=True)
-
-[*cols, type] = combined.columns
-combined = combined[[type, *cols]]
-
-combined.to_csv("./data/result_split.csv", index=False)
+X_train.to_csv("./data/result_train.csv", index=False)
+X_test.to_csv("./data/result_test.csv", index=False)
